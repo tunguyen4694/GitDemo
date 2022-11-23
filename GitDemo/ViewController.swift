62f81d71868b32151baf6aeae098036641dfc1df
TuNA <tu.nguyenanh@mor.com.vn>
2022-11-23 11:07:43 +0700

    set image

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemMint
+        img.image = UIImage(named: "mor")
+        img.contentMode = .scaleAspectFill
     }
 
 
